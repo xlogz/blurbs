73164c7285bbfe43b1419c2b3c1f5bf94912a96e
Logan <loganxlow@gmail.com>
2020-01-05 12:27:18 -0800

    Added categories to registration process

--- public/app/authentication/register.js
+++ public/app/authentication/register.js
@@ -1,4 +1,4 @@
-mainApp.controller('registerCtrl', ['$scope', 'angularAuth0', function($scope, angularAuth0){
+mainApp.controller('registerCtrl', ['$scope', 'angularAuth0', '$http', function($scope, angularAuth0, $http){
 
 	$scope.errorMessage = "none";
 
@@ -7,7 +7,6 @@ mainApp.controller('registerCtrl', ['$scope', 'angularAuth0', function($scope, a
 	$scope.credentials.firstname = "";
 	$scope.credentials.lastname = "";
 	$scope.credentials.username = "";
-	$scope.credentials.password = "";
 	$scope.credentials.email = "";
 
 
@@ -15,7 +14,12 @@ mainApp.controller('registerCtrl', ['$scope', 'angularAuth0', function($scope, a
 		console.log('first name' + $scope.credentials.firstname);
 		console.log('last name' + $scope.credentials.lastname);
 		console.log('username' + $scope.credentials.username);
+		var firstname = $scope.credentials.firstname;
+		var lastname = $scope.credentials.lastname;
+		var username = $scope.credentials.username;
+		var email = $scope.credentials.email;
 
+		var allUserInfo = $scope.credentials;
 		angularAuth0.signup({
 			client_id: 'x4QhXO346dfoY4wzVnWttmDAvFbQogAS', 
 			connection: 'Username-Password-Authentication',
@@ -24,9 +28,33 @@ mainApp.controller('registerCtrl', ['$scope', 'angularAuth0', function($scope, a
 			password: $scope.credentials.password,
 			user_metadata: {firstname: $scope.credentials.firstname, lastname: $scope.credentials.lastname}
 
+
 		}, function(err){
-			if(err) return console.log(err)
-			return alert('success signup without login')
+			if(err) {return console.log(err)}
+			else{
+				console.log('sending put request');
+				console.log(firstname+ '' + lastname + username + email);
+				var userInfo = {
+					firstName: firstname,
+					lastName: lastname,
+					username: username,
+					email: email}
+				}
+
+			 	$http({
+					method: 'PUT',
+					url: 'users/signup',
+					headers: {
+						'Content-Type' : "application/json"
+					},
+					data: {'credentials' : allUserInfo}
+				}).then( function(response){
+					console.log('success sign up and added to database');
+					console.log(response);
+					return response;
+				})
+				
+			
 		});
 	}
 
