e230fc036ca9e07c5c9ae029f95a17d2237692be
Logan <loganxlow@gmail.com>
2020-01-05 12:24:41 -0800

    Updated routes (currently not using)

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,7 @@
-module.exports = function(app){
-	var user = require('../controller/controller.js');
+var user = require('../controller/controller.js');
 
+module.exports = function(app){
 	app.route('/users/bookmarks').get(user.getUserBookmarks);
-	app.route('/users/bookmarks').put(user.putUserBookmarks);
+	app.route('/users/bookmarks').put(user.putUserBookmark);
+	app.route('/users/signup').post(user.signup);
 }
