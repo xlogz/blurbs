d36eb73d4710ae344e814c59d95332042692a1f5
Logan <loganxlow@gmail.com>
2020-01-13 12:10:56 -0800

    Added search bar

--- public/app/addblurb/addBlurb.js
+++ public/app/addblurb/addBlurb.js
@@ -1,7 +1,33 @@
-mainApp.controller('addBlurbCtrl', ['$scope', '$http', 'authService', '$state', function($scope, $http, authService, $state){
+mainApp.controller('addBlurbCtrl', ['$scope', '$http', 'authService', 'blurbService', '$state', '$cookies', '$rootScope', function($scope, $http, authService, blurbService, $state, $cookies, $rootScope){
 	if(!authService.isAuthenticated()){
 		$state.go('noaccess');
 	}else{
 		console.log('user is logged in and can access page');
+
+
+		var authCookie = $cookies.get('auth');
+
+		authService.validateToken(authCookie, function(username){
+	 	$scope.username = $rootScope.user || username;
+
+	 	$scope.userid = username.data[0].userid;
+
+	 	console.log(username)
+
+	 	console.log('this is the users ID');
+	 	console.log($scope.userid);
+	 	
+	 	blurbService.getCategories(username, function(results){
+	 		$scope.categories = results.categories;
+	 		$scope.categiesList = results.categoriesList;
+	 		$scope.username = username;
+	 		$rootScope.username = username;
+	 		console.log('this is the user Obj after getting categories');
+	 		console.log($scope.userObject);
+
+	 	
+	 })
+
+	})
 	}
 }])
